ad28dbb5fccc83fb81171026aa27b1439f901f5d
luciagore <lucy.gore@hotmail.co.uk>
2018-07-20 13:59:42 +0100

    First test passing - create empty grid to begin game

--- spec/GridSpec.js
+++ spec/GridSpec.js
@@ -7,4 +7,8 @@ describe('Grid', function(){
 		grid = new Grid();
 	});
 
+    it("Should have an array of 3 subarrays and start the game empty", function() {
+      expect(grid.current).toEqual([["","",""],["","",""],["","",""]]);
+    });
+
 });
