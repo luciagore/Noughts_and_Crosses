be3e82c25562f1cc4aea46157a7d3879c358487e
luciagore <lucy.gore@hotmail.co.uk>
2018-07-20 14:36:15 +0100

    Add X or O to grid depending on the call

--- spec/GridSpec.js
+++ spec/GridSpec.js
@@ -11,4 +11,15 @@ describe('Grid', function(){
       expect(grid.current).toEqual([["","",""],["","",""],["","",""]]);
     });
 
+		it("Should change a grid space to X when called", function() {
+			grid.addX(0, 0)
+      expect(grid.current).toEqual([["X","",""],["","",""],["","",""]]);
+    });
+
+		it("Should change a grid space to O when called", function() {
+			grid.addX(0, 0)
+			grid.addO(0, 1)
+			expect(grid.current).toEqual([["X","O",""],["","",""],["","",""]]);
+		});
+
 });
--- src/Grid.js
+++ src/Grid.js
@@ -1,3 +1,11 @@
 function Grid(){
  this.current = [["", "", ""], ["", "", ""], ["", "", ""]];
 }
+
+Grid.prototype.addX = function (row, column) {
+	this.current[row][column] = "X"
+};
+
+Grid.prototype.addO = function (row, column) {
+	this.current[row][column] = "O"
+};
