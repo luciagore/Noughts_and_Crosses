6694c71e997bf004d327be05516a64e8a56a0f02
luciagore <lucy.gore@hotmail.co.uk>
2018-07-24 22:06:36 +0100

    Can update table and check if there is a winner in interface

--- src/interface.js
+++ src/interface.js
@@ -14,21 +14,10 @@ $(document).ready(function() {
     $('#player2').text(name1);
   })
 
-  function updateGrid() {
-  $('#00').text(player._grid[0][0]);
-  $('#01').text(player._grid[0][1]);
-  $('#02').text(player._grid[0][2]);
-  $('#10').text(player._grid[1][0]);
-  $('#11').text(player._grid[1][1]);
-  $('#12').text(player._grid[1][2]);
-  $('#20').text(player._grid[2][0]);
-  $('#21').text(player._grid[2][1]);
-  $('#22').text(player._grid[2][2]);
-  }
-
   $('#0-0').click(function() {
     player.playerX(0,0);
     updateGrid();
+    eachTurn();
   });
 
   $('#0-1').click(function() {
@@ -36,5 +25,30 @@ $(document).ready(function() {
     updateGrid();
   });
 
+  $('#0-2').click(function() {
+    player.playerX(0,2);
+    updateGrid();
+  });
+
+
+  function updateGrid() {
+    $('#00').text(player._grid.current[0][0]);
+    $('#01').text(player._grid.current[0][1]);
+    $('#02').text(player._grid.current[0][2]);
+    $('#10').text(player._grid.current[1][0]);
+    $('#11').text(player._grid.current[1][1]);
+    $('#12').text(player._grid.current[1][2]);
+    $('#20').text(player._grid.current[2][0]);
+    $('#21').text(player._grid.current[2][1]);
+    $('#22').text(player._grid.current[2][2]);
+  }
+
+  function eachTurn() {
+    player._grid.checkWin();
+    if(player._grid.winStatus === true) {
+      player._grid.clear();
+      updateGrid();
+    }
+  }
 
   })
