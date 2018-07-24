f3a32196ad35843604b8702586d365d232536fa7
luciagore <lucy.gore@hotmail.co.uk>
2018-07-24 20:51:37 +0100

    Started building interface and html files

---
+++ src/interface.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+  var player = new Player();
+
+  $('#submitXname').submit(function(event) {
+    event.preventDefault();
+    var name = $('#playerX').val();
+    $('#player1').text(name);
+  })
+
+  function updateGrid() {
+  $('#00').text(player._grid[0][0]);
+  $('#01').text(player._grid[0][1]);
+  $('#02').text(player._grid[0][2]);
+  $('#10').text(player._grid[1][0]);
+  $('#11').text(player._grid[1][1]);
+  $('#12').text(player._grid[1][2]);
+  $('#20').text(player._grid[2][0]);
+  $('#21').text(player._grid[2][1]);
+  $('#22').text(player._grid[2][2]);
+  }
+
+  $('#0-0').click(function() {
+    player.playerX(0,0);
+    updateGrid();
+  });
+
+  $('#0-1').click(function() {
+    player.playerX(0,1);
+    updateGrid();
+  });
+
+
+  })
