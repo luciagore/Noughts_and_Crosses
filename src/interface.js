1ee104b1c1da56f1a829eee855a851b7223fe391
luciagore <lucy.gore@hotmail.co.uk>
2018-07-24 21:04:40 +0100

    Can update table with 'X'

--- src/interface.js
+++ src/interface.js
@@ -15,15 +15,15 @@ $(document).ready(function() {
   })
 
   function updateGrid() {
-  $('#00').text(player._grid[0][0]);
-  $('#01').text(player._grid[0][1]);
-  $('#02').text(player._grid[0][2]);
-  $('#10').text(player._grid[1][0]);
-  $('#11').text(player._grid[1][1]);
-  $('#12').text(player._grid[1][2]);
-  $('#20').text(player._grid[2][0]);
-  $('#21').text(player._grid[2][1]);
-  $('#22').text(player._grid[2][2]);
+  $('#00').text(player._grid.current[0][0]);
+  $('#01').text(player._grid.current[0][1]);
+  $('#02').text(player._grid.current[0][2]);
+  $('#10').text(player._grid.current[1][0]);
+  $('#11').text(player._grid.current[1][1]);
+  $('#12').text(player._grid.current[1][2]);
+  $('#20').text(player._grid.current[2][0]);
+  $('#21').text(player._grid.current[2][1]);
+  $('#22').text(player._grid.current[2][2]);
   }
 
   $('#0-0').click(function() {
@@ -33,6 +33,41 @@ $(document).ready(function() {
 
   $('#0-1').click(function() {
     player.playerX(0,1);
+    updateGrid();
+  });
+
+  $('#0-2').click(function() {
+    player.playerX(0,2);
+    updateGrid();
+  });
+
+  $('#1-0').click(function() {
+    player.playerX(1,0);
+    updateGrid();
+  });
+
+  $('#1-1').click(function() {
+    player.playerX(1,1);
+    updateGrid();
+  });
+
+  $('#1-2').click(function() {
+    player.playerX(1,2);
+    updateGrid();
+  });
+
+  $('#2-0').click(function() {
+    player.playerX(2,0);
+    updateGrid();
+  });
+
+  $('#2-1').click(function() {
+    player.playerX(2,1);
+    updateGrid();
+  });
+
+  $('#2-2').click(function() {
+    player.playerX(2,2);
     updateGrid();
   });
 
